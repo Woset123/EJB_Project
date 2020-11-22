b0e422b69d648903849f8a5ce238a542b85ca430
woset <ericdall18@gmail.com>
2020-11-22 11:24:50 +0100

    update code sql

--- code.sql
+++ code.sql
@@ -1,3 +1,5 @@
+
+-- Init
 CREATE TABLE Department (
 id_department DECIMAL ( 3 , 0 ) PRIMARY KEY ,
 name VARCHAR ( 25 ),
@@ -22,7 +24,28 @@ INSERT INTO department VALUES (2,'Customer Support', 3 , 1, 2);
 INSERT INTO department VALUES (3,'Prosthesis', 1 , 2, 1);
 INSERT INTO department VALUES (4, 'TestAssign', 2, 3, 3);
 
+INSERT INTO employee VALUES (222, 1 , 'Michel', 'Jean', 54);
+INSERT INTO employee VALUES (333, 2 , 'Philippe', 'Chandonnet', 34);
+INSERT INTO employee VALUES (444, 2 , 'Etienne', 'Bellemare', 39);
+INSERT INTO employee VALUES (555, 3 , 'Patricia', 'Authier', 42);
+INSERT INTO employee VALUES (777, 0 , 'Too', 'Young' , 12);
+INSERT INTO employee VALUES (888, 0 , 'Too', 'Old', 90);
+INSERT INTO employee VALUES (445, 4 , 'Bernard', 'Toto', 50);
+INSERT INTO employee VALUES (446, 4 , 'Marie', 'Toto', 45);
+INSERT INTO employee VALUES (447, 4 , 'Marion', 'Toto', 48);
+INSERT INTO employee VALUES (448, 0 , 'Toto', 'Toto', 47);
 
+
+-- Reinit
+
+DELETE FROM employee;
+DELETE FROM department;
+
+INSERT INTO department VALUES (0, 'Not Affected', 0 , 0, 0);
+INSERT INTO department VALUES (1,'Sales', 2 , 1, 1);
+INSERT INTO department VALUES (2,'Customer Support', 3 , 1, 2);
+INSERT INTO department VALUES (3,'Prosthesis', 1 , 2, 1);
+INSERT INTO department VALUES (4, 'TestAssign', 2, 3, 3);
 
 INSERT INTO employee VALUES (222, 1 , 'Michel', 'Jean', 54);
 INSERT INTO employee VALUES (333, 2 , 'Philippe', 'Chandonnet', 34);
@@ -34,3 +57,7 @@ INSERT INTO employee VALUES (445, 4 , 'Bernard', 'Toto', 50);
 INSERT INTO employee VALUES (446, 4 , 'Marie', 'Toto', 45);
 INSERT INTO employee VALUES (447, 4 , 'Marion', 'Toto', 48);
 INSERT INTO employee VALUES (448, 0 , 'Toto', 'Toto', 47);
+
+
+
+
